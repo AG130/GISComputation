154d8803d02cd9c04da1e3a1681055841d1edad2
AG130 <q2282536122@163.com>
2022-07-20 17:16:20 +0800

    1

--- hesuan/src/main/main.vue
+++ hesuan/src/main/main.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <h1>1111111</h1>
+        <Map ref="map"/>
     </div>
 </template>
 
 <script>
+import Map from './mapArea/map.vue';
 export default {
-    
+    components: { Map }
 }
 </script>
 
--- hesuan/src/main/mapArea/map.vue
+++ hesuan/src/main/mapArea/map.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>1111111</h1>
+        <h1>mapArea</h1>
     </div>
 </template>
 
