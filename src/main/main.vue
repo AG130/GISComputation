d0e674bf1eb2ba44a02db299030e8866ae337a23
AG130 <q2282536122@163.com>
2022-09-22 12:09:27 +0800

    1

--- src/main/main.vue
+++ src/main/main.vue
@@ -2,21 +2,23 @@
   <div>
     <div>
       <el-container>
+        <!-- 侧边栏 -->
         <el-aside width="200px">
           <div class="asideBand">核酸检测助手</div>
           <div class="personalInf"><PersonalInf ref="personalInf" /></div>
-          <el-menu
-            background-color="#66ccff"
-            active-text-color="white"
-          >
+          <el-menu background-color="#66ccff" active-text-color="white">
             <el-submenu index="1">
               <template v-slot:title>地图功能控件</template>
               <el-menu-item-group>
                 <el-menu-item index="1-1" @click="showMap"
                   >地图展示</el-menu-item
                 >
-                <el-menu-item index="1-2">地图复位</el-menu-item>
-                <el-menu-item index="1-3">地图定位</el-menu-item>
+                <el-menu-item index="1-2" @click="resetMap"
+                  >地图复位</el-menu-item
+                >
+                <el-menu-item index="1-3" @click="showLocatePlace"
+                  >地图定位</el-menu-item
+                >
                 <el-menu-item index="1-4">切换地理底图</el-menu-item>
                 <el-menu-item index="1-5">鹰眼开关</el-menu-item>
               </el-menu-item-group>
@@ -49,29 +51,73 @@
               <template v-slot:title>核酸采样辅助</template>
               <el-menu-item-group>
                 <el-menu-item index="4-1">现有采样点查询</el-menu-item>
-                <el-menu-item index="4-2" @click="testPlaceM">采样点管理</el-menu-item>
+                <el-menu-item index="4-2" @click="testPlaceM"
+                  >采样点管理</el-menu-item
+                >
               </el-menu-item-group>
             </el-submenu>
           </el-menu>
         </el-aside>
+        <!-- 主页面 -->
         <el-container>
+          <!-- 标头 -->
           <el-header>Header</el-header>
+          <!-- 显示区域 -->
           <el-main>
-            <div v-show="this.tab == 0"><Map ref="map" /></div>
+            <div v-show="this.tab == 0"><Map ref="map" @sendCood="getCood" /></div>
             <div v-show="this.tab == 1"><PInfDetail ref="pInfDetail" /></div>
             <div v-show="this.tab == 2">
               <PeopleManage ref="peopleManage" />
             </div>
             <div v-show="this.tab == 3"><DiagnoseP ref="diagnoseP" /></div>
-            <div v-show="this.tab==4"><TestPlace ref="testPlace"/></div>
+            <div v-show="this.tab == 4"><TestPlace ref="testPlace" /></div>
           </el-main>
         </el-container>
       </el-container>
     </div>
+    <!-- 弹窗 -->
     <div>
+      <!-- 地图定位 -->
+      <div>
+        <el-dialog
+          title="地图跳转选择"
+          :visible.sync="locatePlace_vis"
+          :height="'50px'"
+        >
+          <div style="text-align: left">
+            <h3>快速定位</h3>
+            <el-button @click="locatePlace(0)">洪山区</el-button>
+            <el-button @click="locatePlace(1)">东湖高新区</el-button>
+            <el-button @click="locatePlace(2)">南望山校区</el-button>
+            <el-button @click="locatePlace(3)">未来城校区</el-button>
+          </div>
+          <div style="text-align: left">
+            <h3>经纬度定位</h3>
+            <span>当前经纬度为：{{map_center[0]}},{{map_center[1]}}</span>
+            <el-form :model="locatePlace_form">
+              <el-form-item label="请输入经度" :label-width="'85px'">
+                <el-input
+                  v-model="locatePlace_form.x"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="请输入纬度" :label-width="'85px'">
+                <el-input
+                  v-model="locatePlace_form.y"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="locatePlace_vis = false">取消</el-button>
+            <el-button @click="locatePlace(10)">确定</el-button>
+          </div>
+        </el-dialog>
+      </div>
       <!-- 未作核酸人员查询 -->
       <div>
-        <el-dialog title="未检核酸人员查询" :visible.sync="this.p_s_vis">
+        <el-dialog title="未检核酸人员查询" :visible.sync="p_s_vis">
           <el-form :model="p_s_form">
             <el-form-item label="连续未做核酸天数：" :label-width="p_s_width">
               <el-input v-model="p_s_form.data" autocomplete="off"></el-input>
@@ -95,34 +141,79 @@ import PeopleManage from "./mainArea/peopleManage.vue";
 import DiagnoseP from "./mainArea/diagnoseP.vue";
 import TestPlace from "./mainArea/testPlace.vue";
 export default {
-  components: { Map, PersonalInf, PInfDetail, PeopleManage, DiagnoseP, TestPlace },
+  components: {
+    Map,
+    PersonalInf,
+    PInfDetail,
+    PeopleManage,
+    DiagnoseP,
+    TestPlace,
+  },
   provide() {
     return {
       username: this.username,
       changeView: this.changeView,
+      locatePlace_form: this.locatePlace_form,
     };
   },
   data() {
     return {
+      //用户名
       username: this.$route.params.username,
+      //页面代码
       tab: 0,
+      //定位窗口可视化
+      locatePlace_vis: false,
+      //定位数据表单
+      locatePlace_form: {
+        x: "",
+        y: "",
+      },
+      //地图中心点
+      map_center:"",
+      //核酸人员搜索可视化
       p_s_vis: false,
+      //核酸人员搜索参数表单
       p_s_form: {
         data: "",
       },
-      p_s_width:"150px",
+      //行宽
+      p_s_width: "150px",
     };
   },
   methods: {
+    //改变主页面显示
     changeView(id) {
       this.tab = id;
     },
+    //地图功能控件->地图展示
     showMap() {
       this.tab = 0;
     },
+    //地图功能控件->地图复位
+    resetMap() {
+      this.$refs.map.resetMap();
+    },
+    //地图功能控件->地图定位->显示表单
+    showLocatePlace() {
+      this.$refs.map.sendCood();
+      this.locatePlace_vis = true;
+    },
+    //地图功能控件->地图定位->定位区域
+    locatePlace(id) {
+      this.$refs.map.locatePlace(id);
+      this.locatePlace_vis = false;
+      this.locatePlace_form.x = "";
+      this.locatePlace_form.y = "";
+    },
+    getCood(msg){
+      this.map_center=msg
+    },
+    //核酸人员管理->人员录入
     peopleManage() {
       this.tab = 2;
     },
+    //核酸人员管理->未检核酸人员查询
     showSearchPeople() {
       if (this.tab != 0) {
         this.tab = 0;
@@ -131,16 +222,19 @@ export default {
         this.p_s_vis = true;
       }
     },
+    //轨迹分析->阳性轨迹点展示
     showDiagnoseP() {
       this.tab = 3;
     },
-    testPlaceM(){
+    //核酸采样辅助->采样点管理
+    testPlaceM() {
       this.tab = 4;
     },
-    conf_p_s(){
+    //核酸人员管理->未检核酸人员查询->确认查询
+    conf_p_s() {
       // 查询未检核酸人员
-      alert("查询完成")
-    }
+      alert("查询完成");
+    },
   },
 };
 </script>
--- src/main/mainArea/map.vue
+++ src/main/mainArea/map.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <div id="mapDiv"></div>
+    <div id="mapDiv">
+      <div
+        id="mouseP"
+        style="
+          color: #fff;
+          position: absolute;
+          bottom: 10px;
+          right: 10px;
+          z-index: 10000000;
+          width: 200px;
+          line-height: 30px;
+          background: rgba(0, 0, 0, 0.5);
+        "
+      ></div>
+    </div>
     <div>
       <el-button @click="addPoint()">添加点</el-button>
       <el-button @click="addLine()">添加线</el-button>
@@ -11,18 +25,23 @@
  
 <script>
 import "ol/ol.css";
+import OSM from "ol/source/OSM";
 import { Map, View, Feature } from "ol";
 import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
 import { LineString, Point } from "ol/geom";
 import { Style, Fill, Stroke, Circle as sCircle } from "ol/style";
 import { Vector as VectorLayer } from "ol/layer";
 import { Vector as VectorSource } from "ol/source";
+import MousePosition from "ol/control/MousePosition";
+import { createStringXY } from "ol/coordinate";
 
 export default {
+  inject:['locatePlace_form'],
   data() {
     return {
       map: null,
+      initCenter: [114.612, 30.4604],
+      initZoom: 16,
       line_point: [
         [114.625212, 30.46644],
         [114.619551, 30.466218],
@@ -32,8 +51,10 @@ export default {
   },
   mounted() {
     this.initMap();
+    this.initMouse();
   },
   methods: {
+    // 地图初始化
     initMap() {
       this.map = new Map({
         target: "mapDiv",
@@ -46,11 +67,20 @@ export default {
         view: new View({
           // 使用WGS84坐标系
           projection: "EPSG:4326",
-          center: [114.612, 30.4604],
+          center: this.initCenter,
           // 初始16倍
-          zoom: 16,
+          zoom: this.initZoom,
         }),
       });
+    },
+    initMouse() {
+      var MousePositionControl = new MousePosition({
+        coordinateFormat: createStringXY(6),
+        projection: "EPSG:4326",
+        className: "custom-mouse-position",
+        target: document.getElementById("mouseP"),
+      });
+      this.map.addControl(MousePositionControl);
     },
     addPoint() {
       var new_style = new Style({
@@ -94,8 +124,63 @@ export default {
       });
       this.map.addLayer(line);
     },
-    addBuffer(){
-
+    addBuffer() {
+    },
+    // 地图复位
+    resetMap() {
+      var view=this.map.getView();
+      view.animate({
+        center:this.initCenter,
+        zoom:this.initZoom,
+      })
+    },
+    // 地图定位
+    locatePlace(id){
+      var id=id;
+      var view=this.map.getView();
+      switch(id){
+        // 洪山区
+        case 0:
+          view.animate({
+            center:[114.337837,30.502241],
+            zoom:12
+          })
+          break;
+          // 东湖高新区
+        case 1:
+          view.animate({
+            center:[114.525418,30.489736],
+            zoom:12
+          })
+          break;
+          // 南望山
+        case 2:
+          view.animate({
+            center:[114.395896,30.521557],
+            zoom:16
+          })
+          break;
+          // 未来城
+        case 3:
+          view.animate({
+            center:[114.612, 30.4604],
+            zoom:16
+          })
+          break;
+        default:
+          view.animate({
+            center:[this.locatePlace_form.x,this.locatePlace_form.y],
+            zoom:12
+          })
+          break;
+      }
+    },
+    //传地图坐标
+    sendCood(){
+      var mapCenter=this.map.getView().getCenter()
+      mapCenter[0]=mapCenter[0].toFixed(4);
+      mapCenter[1]=mapCenter[1].toFixed(4);
+      this.$emit("sendCood",mapCenter)
     }
   },
 };
@@ -105,5 +190,30 @@ export default {
 #mapDiv {
   width: 100%;
   height: 400px;
+}
+
+#mapCon .ol-zoom .ol-zoom-out {
+  margin-top: 204px;
+}
+#mapCon .ol-zoomslider {
+  background-color: transparent;
+  top: 2.3em;
+}
+#mapCon .ol-touch .ol-zoom .ol-zoom-out {
+  margin-top: 212px;
+}
+#mapCon .ol-touch .ol-zoomslider {
+  top: 2.75em;
+}
+#mapCon .ol-zoom-in .ol.has-tooltip:hover[role="tooltip"],
+#mapCon .ol-zoom-in .ol-has-tooltip:focus[role="tooltip"] {
+  top: 3px;
+}
+#mapCon .ol-zoom-out .ol-has-tooltip:hover[role="tooltip"],
+#mapCon .ol-zoon-out .ol-has-out-tooltip:focus[role="tooltip"] {
+  top: 232px;
+}
+#mapCon .ol-zoom-extent {
+  top: 280px;
 }
 </style>
